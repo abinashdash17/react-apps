d249efb67ba1ecbd49a5f85f10469d0975d60969
Abinash Dash <abinashdash17@gmail.com>
2022-05-02 02:52:39 +0530

    a clean app to start with

--- src/index.js
+++ src/index.js
@@ -2,22 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-let val = 5;
-const reRender = () => {
-    root.render(
-        <div>
-            <div>
-                value {val}
-            </div>
-            <button onClick={
-                () => {
-                    val++;
-                    console.log(val);
-                    reRender();
-                }
-            }>button</button>
-        </div>
-    )
-};
 
-reRender();
+root.render(
+    <>
+    <div>
+        hello
+    </div>
+    </>
+);
