bbab9c46dfb7399f661167b8b09d1a88a021bda6
Abinash Dash <abinashdash17@gmail.com>
2022-05-02 03:11:04 +0530

    use state example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import { React, useState } from "react";
 
 function App(props) {
+    const [val, setVal] = useState(0);
     return (
         <div>
-            test
+            test {val}
+            <button
+                onClick={() => setVal((val) => (val+1))}
+            >
+                change count
+            </button>
         </div>
     )
 }
